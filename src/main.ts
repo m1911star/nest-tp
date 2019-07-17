2b4317b536926e3c9539328cb9960178d88654a3
lihongliang <lhlxtl@gmail.com>
2019-07-17 11:52:08 +0800

    调整目录结构

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+  // if (module.hot) {
+  //   module.hot.accept();
+  //   module.hot.dispose(() => app.close());
+  // }
 }
 bootstrap();
