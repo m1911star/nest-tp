38e3a24b07f1538e2d9e8e6a6c98441cf3c7d3b9
m1911star <lhlxtl@gmail.com>
2021-07-30 00:16:08 +0800

    更新模板依赖

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module, HttpModule } from '@nestjs/common';
-import { AppController } from '@controller/app.controller';
-import { AppService } from '@service/app.service';
+import { AppController } from './controller/app.controller';
+import { AppService } from './service/app.service';
+
 // import { UsersModule } from '@modules/users/users.module';
 
 @Module({
@@ -8,4 +9,5 @@ import { AppService } from '@service/app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 declare const module: any;
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
